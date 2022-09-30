a17f12efa27bcbdde2e125767a6404f2181b623c
Кристина Гуляева <kristinagul.24@gmail.com>
2022-09-30 17:51:39 +1000

    лалала

---
+++ Window10.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1_Практическая_2_Гуляева_Кристина_402
+{
+    /// <summary>
+    /// Логика взаимодействия для Window10.xaml
+    /// </summary>
+    public partial class Window10 : Window
+    {
+        public Window10()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Button1.Background = Brushes.Green;
+        }
+    }
+}
